0e744817d8b3ee0c04f2bc91f0f40042c314b8f6
Ivan Maric <ivan.maric@infinum.hr>
2020-09-22 08:09:48 +0200

    Disable usage of preference library

--- trustkit/src/main/java/com/datatheorem/android/trustkit/utils/VendorIdentifier.java
+++ trustkit/src/main/java/com/datatheorem/android/trustkit/utils/VendorIdentifier.java
@@ -2,12 +2,10 @@ package com.datatheorem.android.trustkit.utils;
 
 
 import android.content.Context;
-import android.content.SharedPreferences;
-
-import androidx.annotation.NonNull;
-import androidx.preference.PreferenceManager;
 
 import java.util.UUID;
+
+import androidx.annotation.NonNull;
 
 /**
  * When TrustKit sends a report, it also sends a randomly-generated identifier to uniquely identify
@@ -20,18 +18,18 @@ public class VendorIdentifier {
 
     @NonNull
     public static String getOrCreate(@NonNull Context appContext) {
-        SharedPreferences trustKitSharedPreferences =
-                PreferenceManager.getDefaultSharedPreferences(appContext);
-        // We store the vendor ID in the App's preferences
-        String appVendorId = trustKitSharedPreferences.getString(TRUSTKIT_VENDOR_ID, "");
-        if (appVendorId.equals("")) {
-            // First time the App is running: generate and store a new vendor ID
-            TrustKitLog.i("Generating new vendor identifier...");
-            appVendorId = UUID.randomUUID().toString();
-            SharedPreferences.Editor editor = trustKitSharedPreferences.edit();
-            editor.putString(TRUSTKIT_VENDOR_ID, appVendorId);
-            editor.apply();
-        }
-        return appVendorId;
+//        SharedPreferences trustKitSharedPreferences =
+//                PreferenceManager.getDefaultSharedPreferences(appContext);
+//        // We store the vendor ID in the App's preferences
+//        String appVendorId = trustKitSharedPreferences.getString(TRUSTKIT_VENDOR_ID, "");
+//        if (appVendorId.equals("")) {
+//            // First time the App is running: generate and store a new vendor ID
+//            TrustKitLog.i("Generating new vendor identifier...");
+//            appVendorId = UUID.randomUUID().toString();
+//            SharedPreferences.Editor editor = trustKitSharedPreferences.edit();
+//            editor.putString(TRUSTKIT_VENDOR_ID, appVendorId);
+//            editor.apply();
+//        }
+        return UUID.randomUUID().toString();
     }
 }
